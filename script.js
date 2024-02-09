80043a67a5647eb7408fe2a7ef6d9ef2ef3f3bec
Mihai1481 <mihaieduard1481@gmail.com>
2024-02-09 22:06:44 +0200

    Add modes to draw  & clear buttons

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-function makeBoard(size){
-let board = document.querySelector(".board")
-let square = board.querySelectorAll("div")
-square.forEach(div => div.remove())
-board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+let color = "black";
 
+function makeBoard(size) {
+  let board = document.querySelector(".board");
+  let square = board.querySelectorAll("div");
+  square.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 1 ; i <= size*size ; i++){
- let square = document.createElement('div')
- square.style.backgroundColor = "red"
- board.insertAdjacentElement("beforeend", square)
- square.addEventListener("mouseover", () =>
- square.style.backgroundColor = "black")
+  for (let i = 1; i <= size * size; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
-
-}
-
 
 makeBoard(16);
 
-
-function changeSize(input){
-    if (input >= 2 && input <= 100){
-        makeBoard(input);
-    } else {
-        console.log("To many squares");
-    }
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    makeBoard(input);
+  } else {
+    console.log("To many squares");
+  }
 }
 
+function colorSquare() {
+  if (color === "rainbow") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+  updateColorText();
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let square = board.querySelectorAll("div");
+  square.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+
+function updateColorText(){
+    if (color === "black"){
+        document.querySelector('.colorSelected').textContent = "Drawing with : Black"
+    } else if (color === "white"){
+        document.querySelector('.colorSelected').textContent = "Selected : Erasor"
+    }  else if (color === "rainbow"){
+        document.querySelector('.colorSelected').textContent = "Drawing with : Rainbow color"
+    }
+}
