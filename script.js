6d39ee96bb8bf0dd9160631098a324bd267c4499
Mihai1481 <mihaieduard1481@gmail.com>
2024-02-09 16:02:27 +0200

    Implement hover effect for color change on div's and introduce javascript functionality for Set a size button

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+function makeBoard(size){
 let board = document.querySelector(".board")
-
-let size = 16
-
+let square = board.querySelectorAll("div")
+square.forEach(div => div.remove())
 board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
 board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
 
-for (let i = 0 ; i < size*size ; i++){
+for (let i = 1 ; i <= size*size ; i++){
  let square = document.createElement('div')
- square.style.backgroundColor = 'red';
+ square.style.backgroundColor = "red"
  board.insertAdjacentElement("beforeend", square)
+ square.addEventListener("mouseover", () =>
+ square.style.backgroundColor = "black")
 }
+
+}
+
+
+makeBoard(16);
+
+
+function changeSize(input){
+    if (input >= 2 && input <= 100){
+        makeBoard(input);
+    } else {
+        console.log("To many squares");
+    }
+}
+
